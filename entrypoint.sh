40260319fbb30427eafa78aa51bf48ff7697dbd3
aiden <83623074+aiden-watcha@users.noreply.github.com>
2021-06-16 16:24:02 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hub release create -a ./${RELEASE_FILE_FOLDER}/*-release.aab -m "${TAG_NAME}" ${TARGET_BRANCH} 
+hub release create -a ./${RELEASE_FILE_FOLDER}/*-release.aab -m "${TAG_NAME}" -t "${TARGET_BRANCH}" "${TAG_NAME}" 
